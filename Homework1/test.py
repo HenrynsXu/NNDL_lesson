c48a2796e93f8a59120c198430803d6f1eb50b79
HenrynsXu <116343602+HenrynsXu@users.noreply.github.com>
2023-04-09 16:55:02 +0800

    Update and rename test.ipynb to test.py

--- Homework1/test.py
+++ Homework1/test.py
@@ -3,4 +3,4 @@ from main import load_MNIST
 train_data,val_data,test_data = load_MNIST()
 nn = MLP()
 nn.load('models/model6_l1e4.npz') # model_name
-nn.validate(test_data)/ len(test_data)
+print(nn.validate(test_data)/ len(test_data))
